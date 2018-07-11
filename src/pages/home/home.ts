4c3b694e472e81bca55062b319135b0bbc43f3aa
Krisda Suwankarn <suwankarn@gmail.com>
2018-07-11 22:32:23 +0700

    2018-07-11

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,40 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { WebapiServiceProvider } from '../../providers/webapi-service/webapi-service';
+import { ContentDetailPage} from '../content-detail/content-detail';
 
+@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  userData = {
+    "keyword": ""
+  };
+  res: any;
+  responseNotfound: any;
 
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public webService: WebapiServiceProvider) {
   }
 
+  getItems() {
+    //alert(JSON.stringify(this.userData));
+    this.webService.postData(this.userData, "search.php").then((result) => {
+      this.res = result;
+    }, (error) => {
+      this.responseNotfound = "มีบางอย่างผิดพลาดจาก Server";
+      console.log(error);
+    })
+  }
+
+  searchDetail(cid) {
+    this.navCtrl.push(ContentDetailPage, {
+      cid: cid
+    });
+  } 
 }
